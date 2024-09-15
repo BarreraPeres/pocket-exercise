11f730c5402556279cf975bfd349262faf16cda5
BarreraPeres <gabrielbarreraperes18@gmail.com>
2024-09-15 14:27:53 -0300

    feat: add UI

---
+++ front-end-pocket/src/components/ui/radio-group.tsx
@@ -0,0 +1,29 @@
+import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
+import { CheckCircle2, Circle } from 'lucide-react'
+
+export function RadioGroup(props: RadioGroupPrimitive.RadioGroupProps) {
+  return (
+    <RadioGroupPrimitive.RadioGroup
+      {...props}
+      className="flex flex-col gap-3"
+    />
+  )
+}
+
+export function RadioGroupItem(props: RadioGroupPrimitive.RadioGroupItemProps) {
+  return (
+    <RadioGroupPrimitive.RadioGroupItem
+      {...props}
+      className="group bg-black border border-zinc-900 rounded-lg px-2 py-1 flex items-center justify-between outline-none hover:border-zinc-800 focus-visible:border-yellow-500 focus-visible:ring-4 ring-pink-500/10 data-[state=checked]:bg-pink-500/5 data-[state=checked]:border-yellow-500"
+    />
+  )
+}
+
+export function RadioGroupIndicator() {
+  return (
+    <>
+      <Circle className="size-4 text-zinc-600 group-data-[state=checked]:hidden" />
+      <CheckCircle2 className="size-4 text-yellow-500 hidden group-data-[state=checked]:inline" />
+    </>
+  )
+}
